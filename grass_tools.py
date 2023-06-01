625918699fe2c1b27014c9a59460f37570d49e99
sclaw <klawson1@uvm.edu>
2023-06-01 09:48:28 -0400

    correct grass tooling.  now matches grass_tools_copy.py

--- grass_tools.py
+++ grass_tools.py
@@ -1,22 +1,22 @@
 import os
 import sys
 import subprocess
-from utilities import load_raster
 
 ### https://grasswiki.osgeo.org/wiki/Working_with_GRASS_without_starting_it_explicitly#Python:_GRASS_GIS_7_without_existing_location_using_metadata_only ###
-GRASS7BIN = r"C:\OSGeo4W\bin\grass78.bat"
-startcmd = [GRASS7BIN, '--config', 'path']
+GRASS8BIN = r"C:\Program Files\GRASS GIS 8.2\grass82.bat"
+startcmd = [GRASS8BIN, '--config', 'python_path']
 p = subprocess.run(startcmd, capture_output=True)
 if p.returncode != 0:
     print(p.stderr, "ERROR: Cannot find GRASS GIS 7 start script ({})".format(startcmd))
     sys.exit(-1)
-GISBASE = p.stdout.decode("utf-8").strip('\n\r')
-os.environ['GISBASE'] = GISBASE
-gpydir = os.path.join(GISBASE, "etc", "python")
+gpydir = p.stdout.decode("utf-8").strip('\n\r')
 sys.path.append(gpydir)
-os.environ['GRASS_SH'] = os.path.join(GISBASE, 'msys', 'bin', 'sh.exe')
-import grass.script as gscript
+import grass.script as gs
 import grass.script.setup as gsetup
+
+# if you need to install r.pi (g.extension extension=r.pi)
+# list addons g.extension -a operation=add   
+
 
 
 class GrassSession:
@@ -24,7 +24,7 @@ class GrassSession:
     def __init__(self, gisdb, location, mapset, epsg, ):
         grass_location = os.path.join(gisdb, location)
         if not os.path.exists(grass_location):
-            cmd = f'"{GRASS7BIN}" -c "EPSG:{epsg}" -e "{grass_location}"'
+            cmd = f'"{GRASS8BIN}" -c "EPSG:{epsg}" -e "{grass_location}"'
             r = subprocess.run(cmd, capture_output=True)
             if r.returncode != 0:
                 print(f'ERROR: {r.stderr}')
@@ -33,24 +33,38 @@ class GrassSession:
             else:
                 print(f'Created location {grass_location}')
 
-        gsetup.init(GISBASE, gisdb, location, mapset)
-        gscript.message('Current GRASS GIS 7 environment:')
-        print(gscript.gisenv())
+        gsetup.init(gisdb, location, mapset)
+        gs.message('Current GRASS GIS 7 environment:')
+        print(gs.gisenv())
 
     def geomorphon(self, dem_path, out_path, search=3, skip=0, flat=1, dist=0):
-        print(print(gscript.gisenv()))
-        in_proj = gscript.read_command('g.proj', flags = 'jf')
-
-        gscript.run_command('r.import', input=dem_path, output='DEM', flags='o')
-        gscript.run_command('g.region rast=DEM')
-        gscript.run_command('r.geomorphon', elevation="DEM", forms='geomorphon', search=search, skip=skip, flat=flat, dist=dist)
-        gscript.run_command('r.out.gdal', input='geomorphon', output=out_path)
+        gs.run_command('r.in.gdal', input=dem_path, output='DEM', overwrite=True)
+        gs.run_command('g.region', rast='DEM')
+        gs.run_command('r.geomorphon', elevation="DEM", forms='geomorphon', overwrite=True, search=search, skip=skip, flat=flat, dist=dist)
+        gs.run_command('r.out.gdal', input='geomorphon', type='Int16', overwrite=True, output=out_path)
 
     def reclass(self, in_path, out_path, rules_path):
-        print(print(gscript.gisenv()))
-        
-        gscript.run_command('r.import', input=in_path, output='input', flags='o')
-        gscript.run_command('g.region rast=input')
-        gscript.run_command('r.reclass', input="input", output='reclassed', rules=rules_path)
-        gscript.run_command('r.out.gdal', input='reclassed', output=out_path)
+        gs.run_command('r.in.gdal', input=in_path, output='input', overwrite=True)
+        gs.run_command('g.region', rast='input')
+        gs.run_command('r.reclass', input="input", output='reclassed', rules=rules_path, overwrite=True)
+        gs.run_command('r.out.gdal', input='reclassed', output=out_path, overwrite=True)
+    
+    def clean_geomorphon(self, in_path, out_dir):
+        gs.run_command('r.in.gdal', input=in_path, output='input', overwrite=True)
+        gs.run_command('g.region', rast='input')
+
+        # Majority filter
+        gs.run_command('r.neighbors', input="input", output='maj_filter', method='mode', nprocs=6, overwrite=True)
+        out_path = os.path.join(out_dir, 'maj_filter.tif')
+        gs.run_command('r.out.gdal', input='maj_filter', output=out_path, type='Int16', overwrite=True)
+
+        # Clump
+        gs.run_command('r.clump', input='maj_filter', output='clumped', overwrite=True)
+        out_path = os.path.join(out_dir, 'clumped.tif')
+        gs.run_command('r.out.gdal', input='clumped', output=out_path, overwrite=True)
+
+        # Edge Density
+        gs.run_command('r.reclass.area', input='maj_filter', output='rmarea', value=0.003, mode='lesser', method='rmarea', overwrite=True)
+        out_path = os.path.join(out_dir, 'rmarea.tif')
+        gs.run_command('r.out.gdal', input='rmarea', output=out_path, overwrite=True)
 
