bc88c62a1125c3f33133e3f8fe475dea5be720e8
sclaw <klawson1@uvm.edu>
2023-12-04 09:28:05 -0500

    add stage scaling

--- source/feature_extraction.py
+++ source/feature_extraction.py
@@ -1,8 +1,21 @@
 import numpy as np
 import pandas as pd
+import geopandas as gpd
 import os
 from scipy.ndimage import gaussian_filter1d
 import matplotlib.pyplot as plt
+import json
+
+def scale_stage(reach_data, el_data):
+    # el_data = el_data.iloc[:, 1:]
+    el_scaled_data = el_data.copy()
+    max_stage_equation = lambda da: 5 * (0.26 * (da ** 0.287))
+    reaches = pd.DataFrame({'ReachCode': el_data.columns})
+    reach_data = pd.merge(reach_data, reaches, right_on='ReachCode', left_index=True, how='right')
+    max_stages = max_stage_equation(reach_data['TotDASqKm'].to_numpy())
+    el_scaled_data.iloc[:, :] = el_data.iloc[:, :] / max_stages
+    el_scaled_data.iloc[:, 0] = el_data.iloc[:, 0]
+    return el_scaled_data
 
 def extract_features(run_path):
     # Load data
@@ -11,15 +24,22 @@ def extract_features(run_path):
     working_dir = run_dict['out_directory']
     reach_path = run_dict['reach_meta_path']
     el_path = os.path.join(working_dir, 'el.csv')
+    el_scaled_path = os.path.join(working_dir, 'el_scaled.csv')
     rh_path = os.path.join(working_dir, 'rh.csv')
     rh_prime_path = os.path.join(working_dir, 'rh_prime.csv')
     area_path = os.path.join(working_dir, 'area.csv')
 
-    reach_data = pd.read_csv(reach_path).dropna(axis=0).drop_duplicates(subset=['ReachCode'])
+    reach_data = gpd.read_file(run_dict['reach_path'], ignore_geometry=True)
+    reach_data = reach_data.drop_duplicates(subset=['MergeCode'])
+    reach_data['ReachCode'] = reach_data['MergeCode']
     reach_data['ReachCode'] = reach_data['ReachCode'].astype(np.int64).astype(str)
     reach_data = reach_data.set_index('ReachCode')
     el_data = pd.read_csv(el_path)
     el_data = el_data.dropna(axis=1)
+    if not os.path.exists(el_scaled_path):
+        el_scaled_data = scale_stage(reach_data, el_data)
+        el_scaled_data.to_csv(el_scaled_path)
+    el_scaled_data = pd.read_csv(el_scaled_path)
     rh_data = pd.read_csv(rh_path)
     rh_data = rh_data.dropna(axis=1)
 
@@ -123,52 +143,56 @@ def extract_features(run_path):
         # z = np.polyfit(tmp_el, tmp_rh_prime, 3)
         # p = np.poly1d(z)
 
-        # fig, ax = plt.subplots()
+        plot = False
+        if plot:
+            fig, ax = plt.subplots()
 
-        # # Plot features
-        # ax.scatter(min_val, el_argmin, ec='r', fc='none', s=25)
-        # ax.text(min_val, el_argmin, 'Min', va='top', ha='right')
+            # Plot features
+            ax.scatter(min_val, el_argmin, ec='r', fc='none', s=25)
+            ax.text(min_val, el_argmin, 'Min', va='top', ha='right')
 
-        # ax.axvline(ave, c='r', ls='dashed', lw=1)
-        # ax.text(ave, tmp_el.max(), 'Average', va='top', ha='right', rotation=90)
+            ax.axvline(ave, c='r', ls='dashed', lw=1)
+            ax.text(ave, tmp_el.max(), 'Average', va='top', ha='right', rotation=90)
 
-        # ax.axhline(start_el, c='r', ls='dotted', lw=1)
-        # ax.text(-3, start_el, 'EDAP', va='bottom', ha='left')
+            ax.axhline(start_el, c='r', ls='dotted', lw=1)
+            ax.text(-3, start_el, 'EDAP', va='bottom', ha='left')
 
-        # ax.axhline(el_bathymetry, c='r', ls='dotted', lw=1)
-        # ax.text(-3, el_bathymetry, 'Bathymetry', va='bottom', ha='left')
+            ax.axhline(el_bathymetry, c='r', ls='dotted', lw=1)
+            ax.text(-3, el_bathymetry, 'Bathymetry', va='bottom', ha='left')
 
-        # ax.axhline(stop_el, c='r', ls='dotted', lw=1)
-        # ax.text(-3, stop_el, 'EDEP', va='bottom', ha='left')
+            ax.axhline(stop_el, c='r', ls='dotted', lw=1)
+            ax.text(-3, stop_el, 'EDEP', va='bottom', ha='left')
 
-        # ax.fill_betweenx(tmp_el[start:stop], tmp_rh_prime[start:stop], np.repeat(ave, height_ind), fc='r')
-        # ax.text((min_val + ave) / 2, (tmp_el[start] + tmp_el[stop]) / 2, 'volume', va='center', ha='center')
+            ax.fill_betweenx(tmp_el[start:stop], tmp_rh_prime[start:stop], np.repeat(ave, height_ind), fc='r')
+            ax.text((min_val + ave) / 2, (tmp_el[start] + tmp_el[stop]) / 2, 'volume', va='center', ha='center')
 
-        # ax.text(ave, (tmp_el[start] + tmp_el[stop]) / 2, 'height ED', va='center', ha='left', rotation=90)
+            ax.text(ave, (tmp_el[start] + tmp_el[stop]) / 2, 'height ED', va='center', ha='left', rotation=90)
 
-        # # ax.plot(p(tmp_el), tmp_el, ls='dashed', c='gray')
+            # ax.plot(p(tmp_el), tmp_el, ls='dashed', c='gray')
 
-        # ax.plot(tmp_rh_prime, tmp_el, c='k')
+            ax.plot(tmp_rh_prime, tmp_el, c='k')
 
-        # plt.title(reach)
-        # ax.set_xlim(-3, 1)
-        # ax.set_ylim(0, tmp_el.max())
-        # ax.set_xlabel(r"$R_{h}$'")
-        # ax.set_ylabel('Stage (m)')
-        # plt.show()
+            plt.title(reach)
+            ax.set_xlim(-3, 1)
+            ax.set_ylim(0, tmp_el.max())
+            ax.set_xlabel(r"$R_{h}$'")
+            ax.set_ylabel('Stage (m)')
+            fig.savefig(r'/netfiles/ciroh/floodplainsData/runs/3/outputs/feature_plots/{}.png'.format(reach))
+            # plt.show()
 
         features.append([reach, ave, min_val, el_bathymetry, argmin, el_argmin, start, start_el, stop, stop_el, height, height_ind, vol, vol_norm, w_bottom, w_edap, w_min, w_edep, rh_edap, rh_min, rh_edep, slope_start_min, slope_min_stop])
 
     # Save
     merge_df = run_dict['muskingum_path']
     merge_df = pd.read_csv(merge_df)
-    merge_df['ReachCode'] = merge_df['ReachCode'].astype(str)
+    merge_df['ReachCode'] = merge_df['ReachCode'].astype(int).astype(str)
 
     out_df = pd.DataFrame(features, columns=['ReachCode', 'ave_rhp', 'min_rhp', 'el_bathymetry', 'ind_min', 'el_min', 'ind_EDAP', 'el_EDAP', 'ind_EDEP', 'el_EDEP', 'height', 'height_ind', 'vol', 'vol_norm', 'w_bottom', 'w_EDAP', 'w_min', 'w_edep', 'rh_edap', 'rh_min', 'rh_edep', 'slope_edap-min', 'slope_min-edep'])
     merge_df = merge_df.merge(out_df, how='inner', on='ReachCode')
-    merge_df.to_csv(run_dict['analysis_path'], index=False)
+    os.makedirs(os.path.dirname(run_dict['analysis_path']), exist_ok=True)
+    # merge_df.to_csv(run_dict['analysis_path'], index=False)
 
 
 if __name__ == '__main__':
-    run_path = r'netfiles\ciroh\floodplainsData\runs\3\run_metadata.json'
+    run_path = r'/netfiles/ciroh/floodplainsData/runs/3/run_metadata_detrended.json'
     extract_features(run_path)
