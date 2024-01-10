c87b594f1719e3ecaada7f6a9a5077b9e84117db
sclaw <klawson1@uvm.edu>
2024-01-10 15:23:48 -0500

    update metadata strings.  Add reach_data export feature.  Enforce reach_data to be subset on reaches within AOI.

--- source/refactor_nhd.py
+++ source/refactor_nhd.py
@@ -7,16 +7,17 @@ import sys
 import sqlite3
 import geopandas as gpd
 import pandas as pd
+import json
 
 
 OGR_PATH = os.path.join(sys.prefix, 'bin', 'ogr2ogr')
 MANUAL_OVERRIDE = {'60000200057445': '4300102000489',
                    '60000200063685': '4300102000489'}
 
-def download_data(save_dir, huc4):
-    url = f'https://prd-tnm.s3.amazonaws.com/StagedProducts/Hydrography/NHDPlusHR/Beta/GDB/NHDPLUS_H_{huc4}_HU4_GDB.zip'
-    zip_path = os.path.join(save_dir, 'NHD.zip')
-    unzip_path = os.path.join(save_dir, 'NHD')
+def download_data(run_dict):
+    url = f'https://prd-tnm.s3.amazonaws.com/StagedProducts/Hydrography/NHDPlusHR/Beta/GDB/NHDPLUS_H_{run_dict["HUC4"]}_HU4_GDB.zip'
+    zip_path = os.path.join(run_dict['network_directory'], 'NHD.zip')
+    unzip_path = os.path.join(run_dict['network_directory'], 'NHD')
 
     print('Starting download')
     r = requests.get(url, stream=True)
@@ -34,10 +35,10 @@ def download_data(save_dir, huc4):
     return out_path
 
 
-def merge_reaches(gdb_path, out_path):
+def merge_reaches(gdb_path, run_dict):
     # merge small DA reaches with d/s reachcode
     print('Exporting VAA table from GDB')
-    subprocess.run([OGR_PATH, '-f', 'SQLite', out_path, gdb_path, 'NHDPlusFlowlineVAA'])
+    subprocess.run([OGR_PATH, '-f', 'SQLite', run_dict['network_db_path'], gdb_path, 'NHDPlusFlowlineVAA'])
 
     in_file = open(os.path.join(os.path.dirname(__file__), 'generate_mainstems.sql'), 'r')
     mainstems = in_file.read().split(';')
@@ -48,7 +49,7 @@ def merge_reaches(gdb_path, out_path):
     in_file.close()
 
     print('Merging reaches and gathering metadata')
-    conn = sqlite3.connect(out_path)
+    conn = sqlite3.connect(run_dict['network_db_path'])
     c = conn.cursor()
 
     [c.execute(q) for q in mainstems]
@@ -62,15 +63,15 @@ def merge_reaches(gdb_path, out_path):
     conn.commit()
 
 
-def clip_flowlines(clip_path, gdb_path, db_path, out_dir):
+def clip_to_study_area(gdb_path, run_dict):
     # Clip flowlines and subcatchments to catchments of interest
     print('Loading Merged VAA table')
-    conn = sqlite3.connect(db_path)
+    conn = sqlite3.connect(run_dict['network_db_path'])
     merged = pd.read_sql_query("SELECT * from merged", conn)
     meta = pd.read_sql_query("SELECT * from reach_data", conn)
 
     print('Loading subbasins')
-    subbasins = gpd.read_file(clip_path)
+    subbasins = gpd.read_file(run_dict['subunit_path'])
 
     print('Loading NHD Flowlines')
     nhd = gpd.read_file(gdb_path, driver='OpenFileGDB', layer='NHDFlowline')
@@ -92,7 +93,7 @@ def clip_flowlines(clip_path, gdb_path, db_path, out_dir):
     intersected = intersected.drop(['length'], axis=1)
 
     print('Saving to file')
-    intersected.to_file(os.path.join(out_dir, 'flowlines.shp'))
+    intersected.to_file(run_dict['flowline_path'])
     intersected = intersected.drop(['geometry'], axis=1)
     
     print('Loading NHD Catchments')
@@ -103,24 +104,35 @@ def clip_flowlines(clip_path, gdb_path, db_path, out_dir):
     nhd = nhd.merge(intersected, how='inner', on='NHDPlusID')
 
     print('Saving to file')
-    nhd.to_file(os.path.join(out_dir, 'catchments.shp'))
+    nhd.to_file(run_dict['reach_path'])
 
+    print('Generating reach metadata table')
+    meta = meta[meta['ReachCode'].isin(nhd['MergeCode'].unique())]
+    meta.to_csv(run_dict['reach_meta_path'], index=False)
 
-def run_all():
-    save_path = r'/users/k/l/klawson1/netfiles/ciroh/slawson/ciroh_network'
-    db_path = os.path.join(save_path, 'vaa.db')
-    clip_path = os.path.join(save_path, 'subunits.shp')
-    gdb_path = r'/users/k/l/klawson1/netfiles/ciroh/slawson/ciroh_network/NHD/NHDPLUS_H_0430_HU4_GDB.gdb'
-    huc4 = '0430'
+def run_all(meta_path, update_metadata=True):
+    # Load run config and initialize directory structure
+    with open(meta_path, 'r') as f:
+        run_dict = json.loads(f.read())
+    if update_metadata:
+        run_dict['network_directory'] = os.path.join(run_dict['run_directory'], 'network')
+        run_dict['network_db_path'] = os.path.join(run_dict['network_directory'], 'vaa.db')
+        run_dict['reach_path'] = os.path.join(run_dict['network_directory'], 'catchments.shp')
+        run_dict['flowline_path'] = os.path.join(run_dict['network_directory'], 'flowlines.shp')
+        run_dict['reach_meta_path'] = os.path.join(run_dict['network_directory'], 'reach_data.csv')
+        with open(meta_path, 'w') as f:
+            json.dump(run_dict, f)
+    os.makedirs(run_dict['network_directory'], exist_ok=True)
 
-    gdb_path = download_data(save_path, huc4)
-    merge_reaches(gdb_path, db_path)
-    clip_flowlines(clip_path, gdb_path, db_path, save_path)
+    gdb_path = download_data(run_dict)
+    merge_reaches(gdb_path, run_dict)
+    clip_to_study_area(gdb_path, run_dict)
 
     print('Cleaning up')
-    shutil.rmtree(os.path.join(save_path, 'NHD'))
-    os.remove(os.path.join(save_path, 'NHD.zip'))
+    shutil.rmtree(os.path.join(run_dict['network_directory'], 'NHD'))
+    os.remove(os.path.join(run_dict['network_directory'], 'NHD.zip'))
 
 
 if __name__ == '__main__':
-    run_all()
+    meta_path = r'/netfiles/ciroh/floodplainsData/runs/3/run_metadata.json'
+    run_all(meta_path)
