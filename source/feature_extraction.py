d1d5df8163d15b2480af251eebf032ffaca66d50
sclaw <klawson1@uvm.edu>
2024-03-26 16:09:31 -0400

    update rhp method with spline

--- source/feature_extraction.py
+++ source/feature_extraction.py
@@ -3,6 +3,7 @@ import pandas as pd
 import geopandas as gpd
 import os
 from scipy.ndimage import gaussian_filter1d
+from scipy.interpolate import splev, splrep
 import matplotlib.pyplot as plt
 import json
 
@@ -10,21 +11,23 @@ import json
 with open('source/regressions.json') as in_file:
     REGRESSIONS = json.load(in_file)
 
-FEATURE_NAMES = ['ReachCode', 'ave_rhp', 'stdev_rhp', 'cumulative_volume', 'cumulative_height', 'valley_confinement', 'el_bathymetry', 'el_edap', 'el_min', 'el_edep', 'el_bathymetry_scaled', 'el_edap_scaled', 'el_min_scaled', 'el_edep_scaled', 'height', 'height_scaled', 'vol', 'vol_scaled', 'min_rhp', 'slope_start_min', 'slope_min_stop', 'rh_bottom', 'rh_edap', 'rh_min', 'rh_edep', 'w_bottom', 'w_edap', 'w_min', 'w_edep', 'invalid_geometry']
+FEATURE_NAMES = ['ReachCode', 'ave_rhp', 'stdev_rhp', 'Ave_Rh', 'cumulative_volume', 'cumulative_height', 'valley_confinement', 'el_bathymetry', 'el_edap', 'el_min', 'el_edep', 'el_bathymetry_scaled', 'el_edap_scaled', 'el_min_scaled', 'el_edep_scaled', 'height', 'height_scaled', 'vol', 'vol_scaled', 'min_rhp', 'slope_start_min', 'slope_min_stop', 'rh_bottom', 'rh_edap', 'rh_min', 'rh_edep', 'w_bottom', 'w_edap', 'w_min', 'w_edep', 'invalid_geometry']
 ERROR_ARRAY = [np.nan for i in FEATURE_NAMES]
 
 class ReachPlot:
 
-    def __init__(self, out_dir, reach) -> None:
+    def __init__(self, out_dir, reach, da, slope) -> None:
         os.makedirs(out_dir, exist_ok=True)
         self.reach = reach
+        self.da = da
+        self.slope = slope
         self.out_path = os.path.join(out_dir, f'{reach}.png')
         self.fig, (self.section_ax, self.rh_ax, self.rhp_ax) = plt.subplots(ncols=3, figsize=(10, 3), sharey=True)
         self.all_ax = (self.section_ax, self.rh_ax, self.rhp_ax)
         self.has_geom = False
 
         # Add labels
-        self.section_ax.set_xlabel('Station (m)', fontsize=10)
+        self.section_ax.set_xlabel('Station (m / bkf_w) (unitless)', fontsize=10)
         self.section_ax.set_ylabel('Stage / Bankfull Depth', fontsize=10)
         self.rh_ax.set_xlabel(r'$R_{h}$', fontsize=10)
         self.rhp_ax.set_xlabel(r"$R_{h}$'", fontsize=10)
@@ -40,6 +43,7 @@ class ReachPlot:
         width = width / 2
         width = np.append(-width[::-1], width)
         width = width - min(width)
+        width = width / (2.44 * (self.da ** 0.34))
         section_el = np.append(el[::-1], el)
 
         # Cache geometry
@@ -67,10 +71,10 @@ class ReachPlot:
             stop = main_edz['stop_ind']
             self.rhp_ax.fill_betweenx(self.el[start:stop], 0.5, self.rhp[start:stop])
 
-    def add_aeps(self, q, da):
+    def add_aeps(self, q):
        for reg in REGRESSIONS['peak_flowrate']:
             params = REGRESSIONS['peak_flowrate'][reg]
-            q_ri = (params[0] * ((da / 2.59) ** params[1])) / 35.3147
+            q_ri = (params[0] * ((self.da / 2.59) ** params[1])) / 35.3147
             norm_stage = np.interp(q_ri, q, self.el)
 
             self.section_ax.axhline(norm_stage, c='c', alpha=0.3, ls='dashed')
@@ -88,7 +92,7 @@ class ReachPlot:
             self.rhp_ax.set(xlim=(-1, 1), ylim=(0, 6))
 
         # Export
-        self.fig.suptitle(self.reach)
+        self.fig.suptitle(f'{self.reach} | {round(self.da, 1)} sqkm | {self.slope} m/m')
         self.fig.tight_layout()
         self.fig.savefig(self.out_path, dpi=dpi)
         plt.close()
@@ -238,11 +242,11 @@ def reprocess_rhp(el, rh, tol=0.01):
     simplified_points = douglas_peucker(points, tol)
     simp_rh = [i[1] for i in simplified_points]
     simp_el = [i[0] for i in simplified_points]
-    rh = np.interp(el, simp_el, simp_rh)
-
-    rhp = (rh[1:] - rh[:-1]) / (el[1:] - el[:-1])
-    rhp = np.append(rhp, rhp[-1])
-    rhp = np.nan_to_num(rhp)
+    el_inc = np.median(el[1:] - el[:-1])
+    el_fit = np.arange(0, max(el), el_inc)
+    rh_fit = np.interp(el_fit, simp_el, simp_rh)
+    spl = splrep(el_fit, rh_fit, t=simp_el[1:-1])
+    rhp = splev(el, spl, der=1)
 
     return rhp
 
@@ -305,13 +309,14 @@ def extract_features(run_path, plot=False, subset=None):
         tmp_el = el_data[reach].to_numpy()
         tmp_el_scaled = el_scaled_data[reach].to_numpy()
         tmp_rh = rh_data[reach].to_numpy()
-        tmp_rh_prime = reprocess_rhp(tmp_el, tmp_rh)
         # tmp_rh_prime = rh_prime_data[reach].to_numpy()
         tmp_area = area_data[reach].to_numpy() / tmp_meta['length']
         tmp_volume = volume_data[reach].to_numpy() / tmp_meta['length']
 
         if plot:
-            reach_plot = ReachPlot(run_dict['geometry_diagnostics'], reach)
+            slope = tmp_meta['slope']
+            da = tmp_meta['TotDASqKm']
+            reach_plot = ReachPlot(run_dict['geometry_diagnostics'], reach, da, slope)
 
         # Error handling
         if np.all(tmp_area == 0):
@@ -322,6 +327,8 @@ def extract_features(run_path, plot=False, subset=None):
             tmp_features[-1] = 1
             features.append(tmp_features)
             continue
+
+        tmp_rh_prime = reprocess_rhp(tmp_el, tmp_rh)
 
         # Process
         thresh = 0.5
@@ -339,6 +346,7 @@ def extract_features(run_path, plot=False, subset=None):
         el_bathymetry_scaled = tmp_el_scaled[bathymetry_break]
         ave = np.nanmean(tmp_rh_prime)
         stdev = np.nanstd(tmp_rh_prime)
+        ave_rh = np.nanmean(tmp_rh)
 
         if edz_count == 0:
             tmp_features = ERROR_ARRAY.copy()
@@ -356,14 +364,13 @@ def extract_features(run_path, plot=False, subset=None):
             main_edz_ind = [i for v, i in sorted(zip(edz_vols, edzs.keys()), reverse=True)][0]
             main_edz = edzs[main_edz_ind]
             valley_confinement = main_edz['w_edep'] / main_edz['w_edap']
-            features.append([reach, ave, stdev, cum_vol, cum_height, valley_confinement, el_bathymetry, main_edz['start_el'], main_edz['min_el'], main_edz['stop_el'], el_bathymetry_scaled, main_edz['start_el_scaled'], main_edz['min_el_scaled'], main_edz['stop_el_scaled'], main_edz['height'], main_edz['height_scaled'], main_edz['volume'], main_edz['vol_scaled'], main_edz['min_val'], main_edz['slope_start_min'], main_edz['slope_min_stop'], main_edz['rh_bottom'], main_edz['rh_edap'], main_edz['rh_min'], main_edz['rh_edep'], main_edz['w_bottom'], main_edz['w_edap'], main_edz['w_min'], main_edz['w_edep'], 0])
+            features.append([reach, ave, stdev, ave_rh, cum_vol, cum_height, valley_confinement, el_bathymetry, main_edz['start_el'], main_edz['min_el'], main_edz['stop_el'], el_bathymetry_scaled, main_edz['start_el_scaled'], main_edz['min_el_scaled'], main_edz['stop_el_scaled'], main_edz['height'], main_edz['height_scaled'], main_edz['volume'], main_edz['vol_scaled'], main_edz['min_val'], main_edz['slope_start_min'], main_edz['slope_min_stop'], main_edz['rh_bottom'], main_edz['rh_edap'], main_edz['rh_min'], main_edz['rh_edep'], main_edz['w_bottom'], main_edz['w_edap'], main_edz['w_min'], main_edz['w_edep'], 0])
 
         if plot:
             reach_plot.add_geometry(tmp_el_scaled, tmp_area, tmp_rh, tmp_rh_prime, ave)
             reach_plot.add_edzs(edzs, main_edz)
             q = (1 / np.repeat(0.07, len(tmp_el))) * tmp_volume * (tmp_rh ** (2 / 3)) * (tmp_meta['slope'] ** 0.5)
-            da = tmp_meta['TotDASqKm']
-            reach_plot.add_aeps(q, da)
+            reach_plot.add_aeps(q)
             reach_plot.save()
 
     # Save
@@ -384,5 +391,4 @@ def extract_features(run_path, plot=False, subset=None):
 if __name__ == '__main__':
     run_path = r'/netfiles/ciroh/floodplainsData/runs/6/run_metadata.json'
     subset = ['4300103004201', '4300103001342', '4300101001766', '4300103004379', '4300103003747', '4300105005363', '4300103001544', '4300107000959', '4300101002118', '4300105002133', '4300103001610', '4300105004505', '4300108010065', '4300101002210', '4300103004018', '4300107002508', '4300107002403', '4300101001930', '4300103001533', '4300103004182', '4300103001950', '4300103005201', '4300103000479', '4300103002798', '4300102002584', '4300103000195', '4300105004967', '4300107001561', '4300107002261', '4300105002860', '4300105001105', '4300103000194', '4300102002233', '4300103001480', '4300102002678', '4300108009146', '4300103001304', '4300103003397', '4300101001605', '4300103001361', '4300108010453', '4300102005480', '4300103003032', '4300105000186', '4300101002194', '4300107002753', '4300103001501', '4300103004766', '4300103002087']
-    subset = None
-    extract_features(run_path, plot=False, subset=subset)
+    extract_features(run_path, plot=False, subset=None)
