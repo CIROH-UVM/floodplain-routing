412328443f52e072879e193076f2c8178c83d212
sclaw <klawson1@uvm.edu>
2024-05-02 18:19:21 -0400

    propogate wbody

--- source/refactor_nhd.py
+++ source/refactor_nhd.py
@@ -201,7 +201,7 @@ def clip_to_study_area(gdb_path, run_dict, water_toggle=0.05):
     meta = meta.merge(subunits, how='left', left_on='ReachCode', right_on=run_dict["id_field"])
     meta[['8_code', run_dict['subunit_field']]] = meta['Code_name'].str.split('_', expand=True)
     meta[run_dict['unit_field']] = meta['8_code'].map(NAME_DICT)
-    meta = meta.merge(wbody_intersect[[run_dict["id_field"], 'wbody']], left_on='ReachCode', right_on=run_dict["id_field"], how='left')
+    meta = meta.merge(wbody_intersect[[run_dict["id_field"], 'wbody']], on=run_dict["id_field"], how='left').fillna(False)
     meta.to_csv(run_dict['reach_meta_path'], index=False)
 
 def run_all(meta_path):
@@ -209,14 +209,13 @@ def run_all(meta_path):
     with open(meta_path, 'r') as f:
         run_dict = json.loads(f.read())
 
-    # gdb_path = download_data(run_dict)
-    gdb_path = r'/netfiles/ciroh/floodplainsData/runs/7/network/NHD/NHDPLUS_H_0430_HU4_GDB.gdb'
-    # merge_reaches(gdb_path, run_dict, merge_short=True)
+    gdb_path = download_data(run_dict)
+    merge_reaches(gdb_path, run_dict, merge_short=True)
     clip_to_study_area(gdb_path, run_dict)
 
     print('Cleaning up')
-    # shutil.rmtree(os.path.join(run_dict['network_directory'], 'NHD'))
-    # os.remove(os.path.join(run_dict['network_directory'], 'NHD.zip'))
+    shutil.rmtree(os.path.join(run_dict['network_directory'], 'NHD'))
+    os.remove(os.path.join(run_dict['network_directory'], 'NHD.zip'))
 
 
 if __name__ == '__main__':
