1b38666f819b6329638c6dccb4b501c193e3f367
sclaw <klawson1@uvm.edu>
2024-08-13 14:38:24 -0400

    set up argparser

--- source/make_run_template.py
+++ source/make_run_template.py
@@ -1,19 +1,24 @@
 import os
 import sys
 import json
+import argparse
 
+### PARSING ###
+parser = argparse.ArgumentParser(description='Make a run metadata file from the template.')
+parser.add_argument('data_directory', type=str, help='Path to folder containing all data.')
+parser.add_argument('run_id', type=str, help='Unique ID for this run. (str)')
 
 def make_run_template(base_directory='/path/to/data', run_id='1'):
     run_metadata = {
-        "data_directory": base_directory,
-        "run_directory": os.path.join(base_directory, 'runs', run_id),
-        "id_field": "UVM_ID",
-        "unit_field": "8_name",
-        "subunit_field": "12_code",
-        "scaled_stages": True,
-        "huc4": "0430",
-        "subunit_path": os.path.join(base_directory, 'shared', 'subunits.shp'),
-        "geometry_source": 'HAND'
+        "data_directory": base_directory,  # Directory containing all data
+        "run_directory": os.path.join(base_directory, 'runs', run_id),  # Subdirectory for this run
+        "id_field": "UVM_ID",  # Field used in several files to uniquely identify each reach
+        "unit_field": "8_name",  # Field used to identify the first level of batch processing.  Typically the HUC8.
+        "subunit_field": "12_code",  # Field used to identify the second level of batch processing.  Typically the HUC12.
+        "scaled_stages": True,  # Whether to use 6xbankfull as the maximum stage (true) or 10 meters (false)
+        "huc4": "0430",  # HUC4 code for this run.  Used to download NHD data.
+        "subunit_path": os.path.join(base_directory, 'shared', 'subunits.shp'),  # Path to shapefile containing subunit boundaries.
+        "geometry_source": 'HAND'  # Source of geometry data.  Either 'HAND' or 'DEM'.
         }
 
     os.makedirs(run_metadata['run_directory'], exist_ok=True)
@@ -23,6 +28,5 @@ def make_run_template(base_directory='/path/to/data', run_id='1'):
 
 
 if __name__ == '__main__':
-    base_directory = sys.argv[1]  # Folder of data directory
-    run_id = sys.argv[2]  # unique identifier for run 
-    make_run_template(base_directory, run_id)
+    args = parser.parse_args()
+    make_run_template(args.data_directory, args.run_id)
