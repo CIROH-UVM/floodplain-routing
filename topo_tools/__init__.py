69afe1092c88b8e7cfc77d97562e860224d4e397
sclaw <klawson1@uvm.edu>
2024-08-14 10:55:11 -0400

    update method name

--- topo_tools/__init__.py
+++ topo_tools/__init__.py
@@ -1,5 +1,5 @@
 from .make_run_template import make_run_template
-from .refactor_nhd import run_all
+from .refactor_nhd import get_reaches
 from .controller import extract_geometry, batch_add_bathymetry
 from .feature_extraction import extract_features
 from .map_tools import map_floodplain, map_edzs
