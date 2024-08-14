d6932e085aab277dc66997c4523391557f224bb5
sclaw <klawson1@uvm.edu>
2024-08-14 09:03:09 -0400

    fix typo

--- topo_tools/__init__.py
+++ topo_tools/__init__.py
@@ -2,5 +2,5 @@ from make_run_template import make_run_template
 from refactor_nhd import run_all
 from controller import extract_geometry, batch_add_bathymetry
 from feature_extraction import extract_features
-from map_tools import map_floodplain, map_edz
+from map_tools import map_floodplain, map_edzs
 from floods import analyze_floods
