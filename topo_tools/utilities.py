bb446ed8440103a71ce8f0d80f66d3c7a035b473
Alexander B. Prescott <alexander.prescott@uvm.edu>
2025-07-08 16:26:51 -0400

    temporary print to check imputed bathymetry stage

--- topo_tools/utilities.py
+++ topo_tools/utilities.py
@@ -294,6 +294,8 @@ def add_bathymetry(geom, da, slope):
     perimeter = perimeter[:channel_ind]
     area_diff = geom['area'][filter_arg] - top_width
 
+    print(stage_space[-1])
+
     geom['area'] = geom['area'][filter_arg:]
     geom['area'] = np.insert(geom['area'], 0, width)
     geom['area'] = geom['area'][:dim]
