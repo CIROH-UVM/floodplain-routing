47b90d0087420d9c969b61406586e5ccf86263d9
Alexander B. Prescott <alexander.prescott@uvm.edu>
2025-12-22 16:33:18 -0500

    temp change for bathymetry investigation

--- topo_tools/utilities.py
+++ topo_tools/utilities.py
@@ -294,7 +294,7 @@ def add_bathymetry(geom, da, slope):
     perimeter = perimeter[:channel_ind]
     area_diff = geom['area'][filter_arg] - top_width
 
-    print(f'Top of imputed channel at {stage_space[-1]}')
+    print(f'Scaled top of imputed channel at {stage_space[-1] / (0.5 * max_space)}')
     geom['area'] = geom['area'][filter_arg:]
     geom['area'] = np.insert(geom['area'], 0, width)
     geom['area'] = geom['area'][:dim]
